0cb6b9edeadc8e2e0de8c116a9f132e79e3603f4
Todd Dukart <todd.dukart@globe.com>
2017-09-15 14:28:14 -0400

    Add loader options

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
 var dot = require('dot');
 var fs = require('fs');
+var loaderUtils = require('loader-utils');
 
 module.exports = function(content) {
   if (this.cacheable) {
     this.cacheable();
   }
-  
+
+  var options = Object.assign(
+    {},
+    {
+      strip: true
+    },
+    loaderUtils.getOptions(this)
+  );
+
   dot.templateSettings.selfcontained = true;
-  dot.templateSettings.strip = false;
+  dot.templateSettings.strip = options.strip;
 
   var content = fs.readFileSync(this.resourcePath);
   return "module.exports = " + dot.template(content);
